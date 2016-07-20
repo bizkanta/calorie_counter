dae5b947cdf558c8c85604da37da8a301f845125
bizkanta <totivan.aliz@gmail.com>
2016-07-20 21:46:50 +0200

    delete meals frontend

--- client/frontend.js
+++ client/frontend.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function(document, Xhr_request){
+(function(document, Xhr_request, moment){
 
   var request = new Xhr_request();
 
@@ -9,39 +9,81 @@
   var calorieInputField = document.querySelector('#calories');
   var dateInputField = document.querySelector('#date');
   var addButton = document.querySelector('.addButton');
+  var deleteButton = document.querySelector('.deleteButton');
+  mealsList.addEventListener('click', selectItem);
+  addButton.addEventListener('click', addNewMealItem);
+  deleteButton.addEventListener('click', deleteSelectedItem);
 
-  addButton.addEventListener('click', newMealItem);
+  function getCurrentDateAsString() {
+    var now = new Date();
+    var string = now.toISOString().replace('Z', '');
+    console.log(string)
+    return '2016-07-20 20:09:47.817'
+    return string;
+  }
+  dateInputField.defaultValue = getCurrentDateAsString();
 
   request.getMealsFromServer(insertItemsToDOM);
 
   function insertItemsToDOM(items) {
     items.forEach(function(item) {
-      appendMeals(item);
+      appendMeal(item);
     });
   }
 
-  function newMealItem(event) {
+  function addNewMealItem(event) {
     event.preventDefault();
     var mealItem = {
       name: nameInputField.value,
       calories: calorieInputField.value,
       date: dateInputField.value
     };
-    request.addMealToServer(mealItem, appendMeals);
+    request.addMealToServer(mealItem, appendMeal);
     nameInputField.value = '';
     calorieInputField.value = '';
     dateInputField.value = '';
     nameInputField.focus();
   }
 
-  function appendMeals(item) {
+  function deleteSelectedItem(event) {
+    var selectedItem = document.querySelector('.selectedMeal');
+    if (selectedItem) {
+      var selectedId = selectedItem.getAttribute('id');
+      request.deleteMealFromServer(selectedId, function() {
+        selectedItem.remove();
+      });
+    }
+  }
+
+  function selectItem(event) {
+    var alreadySelectedItem = document.querySelector('.selectedMeal');
+    if (alreadySelectedItem) {
+      alreadySelectedItem.classList.remove('selectedMeal');
+    }
+    var isMealsContainerClicked = event.target.classList.contains('meals');
+    if (!isMealsContainerClicked) {
+      var selectedRow = findAncestorByClass(event.target, 'mealItem');
+      selectedRow.classList.add('selectedMeal');
+    }
+  }
+
+  function findAncestorByClass(element, classSelector) {
+    if (element.classList.contains(classSelector)) {
+      return element;
+    }
+    return findAncestorByClass(element.parentNode, classSelector);
+  }
+
+  function appendMeal(item) {
+    var date = moment().
+    item.date.substring(0, 10) + ' , ' + item.date.substring(12, 16);
     var mealItem = `
     <div id="${item.id}" class="mealItem">
       <div class="mealName">${item.name}</div>
       <div class="mealCalories">${item.calories}</div>
-      <div class="mealDate">${item.date}</div>
+      <div class="mealDate">${date}</div>
     </div>`;
     mealsList.innerHTML += mealItem;
   }
 
-})(document, Xhr_request);
+})(document, Xhr_request, moment);
--- client/xhr_requests.js
+++ client/xhr_requests.js
@@ -1,16 +1,21 @@
 'use strict';
 
 function Xhr_request(){
-  this.url = 'http://127.0.0.1:3000/meals';
+  this.url = 'http://127.0.0.1:3000/meals/';
 
   this.getMealsFromServer = function(callback){
     this.createRequest('GET', this.url, null, callback);
   };
 
   this.addMealToServer = function(meal, callback){
-  var newItem = JSON.stringify(meal);
-  this.createRequest('POST', this.url, newItem, callback);
-}
+    var newItem = JSON.stringify(meal);
+    this.createRequest('POST', this.url, newItem, callback);
+  }
+
+  this.deleteMealFromServer = function(id, callback){
+    var url = this.url + id;
+    this.createRequest('DELETE', url, null, callback);
+  };
 
   this.createRequest = function(method, url, data, callback){
     var xhr = new XMLHttpRequest();
